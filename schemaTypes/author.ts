106dc792cccafda8f581d3fd4a74e0001b238ed1
Yushan Fernando <yushanwebdev@gmail.com>
2024-06-30 12:34:02 +0530

    feat: Complete the course

--- schemaTypes/author.ts
+++ schemaTypes/author.ts
@@ -11,6 +11,11 @@ export default defineType({
       type: 'string',
     }),
     defineField({
+      name: 'nickname',
+      title: 'NickName',
+      type: 'string',
+    }),
+    defineField({
       name: 'slug',
       title: 'Slug',
       type: 'slug',
